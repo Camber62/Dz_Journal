6090bd9371ed6735afc9d79c2a10569d5bccc046
Camber62 <Camber62@mail.ru>
2022-08-31 21:00:57 +0300

    lesson 31.08

---
+++ ExamJS/Movies.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector('.btn')
+const Age = document.querySelector('.Name').value
+
+btn.addEventListener('click', function click() {
+    // const valueAge = Age;
+    // const reger = /qwerty/i;  //проверка шаблона И проверка на буквы/([а-я][А-Я])+/
+    //
+    // if (reger.test(value)) {
+    //     console.log('da')
+    // } else {
+    //     console.log('no')
+    // }
+
+console.log(Age)
+})
+
+
+
+
+
